195f5e5012e9ef7a563b4c9ce368f5647f71f720
omaritooo <themedic227@gmail.com>
2023-12-02 16:06:56 +0200

    Remove dist folder

--- dist/routes/projectsRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const projectController_1 = require("../controllers/projectController");
-const router = (0, express_1.Router)();
-router.route('/').get(projectController_1.getProjects).post(projectController_1.createProject);
-router.route('/:id').get(projectController_1.getProject).delete(projectController_1.deleteProject).patch(projectController_1.updateProject);
-exports.default = router;
